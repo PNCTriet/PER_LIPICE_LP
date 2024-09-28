95467009ce5f512f0253fda014d88e8b57775895
Triết Nguyễn Phạm <triet.pnc@gmail.com>
2024-09-28 18:37:16 +0700

    update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0 ,maximum-scale=1" />
   <!-- Các thẻ meta khác -->
   <link rel="icon" type="image/png" sizes="64x64" href="../images/metalogo.png" />
   <meta property="og:title" content="LIPICE CÙNG LỌ LEM" />
